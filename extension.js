7ab5b25cae56e1206979d305957a8b298fb7546d
Charlie Weems <charlie.weems@gmail.com>
2019-08-05 01:20:38 -0700

    Complete activation command

--- extension.js
+++ extension.js
@@ -98,9 +98,25 @@ function activate(context) {
 		const terminal = assignTerminal(vscode, 'activate')
 
 		getServiceSid(vscode).then((serviceSid) => {
+			
+			vscode.window.showInputBox({
+				ignoreFocusOut: true,
+				placeHolder: "Enter the environment you want to deploy from (e.g. 'dev')...",
+			}).then(source => {
+				vscode.window.showInputBox({
+					ignoreFocusOut: true,
+					placeHolder: "Enter the environment you want to deploy to (e.g. 'prod')..."
+				}).then(destination => {
 
-			terminal.show();
-			terminal.sendText(`twilio serverless:activate --service-sid=${serviceSid}`);
+					terminal.show();
+					terminal.sendText();
+					terminal.sendText(
+						`twilio serverless:activate --service-sid=${serviceSid} \
+							--environment=${destination} \
+							--source-environment=${source}`
+					);
+				});
+			});
 		});
 	});
 
@@ -125,8 +141,8 @@ function activate(context) {
 							--domain-suffix=${suffix} \
 							--unique-name=${uniqueName}`
 					);
-				})
-			})
+				});
+			});
 
 		});
 	});
