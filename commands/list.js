8224bdc4c0a0090443f6f54a2e41f8c732b8879e
gagandeepp <gagandeeppratihar@outlook.com>
2019-10-01 09:02:18 +0530

    Update list.js
    File name printed on the console

--- commands/list.js
+++ commands/list.js
@@ -1,10 +1,19 @@
-const vscode = require('vscode');
-const assignTerminal = require('../helpers/assignTerminal');
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require('path');
+const assignTerminal = require("../helpers/assignTerminal");
 
 function list() {
-    const terminal = assignTerminal(vscode, 'list')
-    terminal.show();
-    terminal.sendText(`twilio serverless:list`);
+  const terminal = assignTerminal(vscode, "list");
+  terminal.show();
+  terminal.sendText(`twilio serverless:list`);
+  const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  const fnDirPath = path.join(wsPath, "functions");
+  fs.readdir(fnDirPath, (err, files) => {
+    files.forEach(file => {
+      console.log(file);
+    });
+  });
 }
 
 module.exports = list;
