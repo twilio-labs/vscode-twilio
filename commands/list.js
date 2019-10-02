4b2aeec53a9a326481bed34376de251062e3264d
gagandeepp <gagandeeppratihar@outlook.com>
2019-10-02 17:42:19 +0530

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -2,6 +2,9 @@ const vscode = require("vscode");
 const fs = require("fs");
 const path = require('path');
 const assignTerminal = require("../helpers/assignTerminal");
+const { TwilioClientCommand } = require("@twilio/cli-core").baseCommands;
+
+const { handler, cliInfo, describe } = require("twilio-run/dist/commands/list");
 
 function list(type = 'functions') {
   const terminal = assignTerminal(vscode, "list");
