bb72d27014f48f8a76be70e28b2cee80077efccd
gagandeepp <gagandeeppratihar@outlook.com>
2019-10-02 17:31:27 +0530

    review pointer fixed

--- commands/list.js
+++ commands/list.js
@@ -8,12 +8,6 @@ function list(type = 'functions') {
   terminal.show();
   terminal.sendText(`twilio serverless:list`);
   const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
-  const fnDirPath = path.join(wsPath, "functions");
-  fs.readdir(fnDirPath, (err, files) => {
-    files.forEach(file => {
-      console.log(file);
-    });
-  });
 }
 
 module.exports = list;
