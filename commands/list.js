88a876c682f09388fc7ada743e7cc11d7bcc0531
gagandeepp <gagandeeppratihar@outlook.com>
2019-10-03 19:42:47 +0530

    Code review pointer added

--- commands/list.js
+++ commands/list.js
@@ -2,15 +2,11 @@ const vscode = require("vscode");
 const fs = require("fs");
 const path = require('path');
 const assignTerminal = require("../helpers/assignTerminal");
-const { TwilioClientCommand } = require("@twilio/cli-core").baseCommands;
-
-const { handler, cliInfo, describe } = require("twilio-run/dist/commands/list");
 
 function list(type = 'functions') {
   const terminal = assignTerminal(vscode, "list");
   terminal.show();
-  terminal.sendText(`twilio serverless:list`);
-  const wsPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+  terminal.sendText(`twilio serverless:list ${type}`);
 }
 
 module.exports = list;
